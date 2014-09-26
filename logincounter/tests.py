04a39131adf9cf05c045303ee7e7cce07d398e58
pwoned <peuanthinsan@gmail.com>
2014-09-26 13:25:26 -0700

    more unit tests

--- logincounter/tests.py
+++ logincounter/tests.py
@@ -14,7 +14,34 @@ class TestUsers(TestCase):
 		
     def testLogin(self):
     	response = self.client.post('/users/login/', data = json.dumps({'user': self.user1.user, 'password': self.user1.password}), content_type="application/json")
-    	print response.content
+    	result = json.loads(response.content)
+    	user1 = User.objects.get(user=self.user1.user)
+    	
     	self.assertEquals(200, response.status_code)
+    	self.assertEquals(user1.login_count, result['count'])
+    	self.assertEquals(User.SUCCESS, result['errCode'])
+    	
+    def testDuplicateUser(self):
+    	response = self.client.post('/users/add/', data = json.dumps({'user': self.user1.user, 'password': 'wrong password'}), content_type="application/json")
+    	result = json.loads(response.content)
+
+    	user1 = User.objects.get(user=self.user1.user) 
+    		
+    	self.assertEquals(200, response.status_code)
+    	self.assertEquals(self.user1.password, user1.password)
+    	self.assertEquals(User.ERR_USER_EXISTS, result['errCode'])
+   	
+    def testInvalidLogin(self):
+    	response = self.client.post('/users/login/', data = json.dumps({'user': self.user1.user, 'password': 'wrong password'}), content_type="application/json")
+    	result = json.loads(response.content)
+    	
+    	self.assertEquals(200, response.status_code)
+    	self.assertEquals(User.ERR_BAD_CREDENTIALS, result['errCode'])
 		
+    def testInvalidPassword(self):
+    	response = self.client.post('/users/login/', data = json.dumps({'user': self.user1.user, 'password': 'wrong password'}), content_type="application/json")
+    	result = json.loads(response.content)
+    	
+    	self.assertEquals(200, response.status_code)
+    	self.assertEquals(User.ERR_BAD_CREDENTIALS, result['errCode'])
 	
