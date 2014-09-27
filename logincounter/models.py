47715d029413764ccf3bd8f006be0680c2cadbf7
pwoned <peuanthinsan@gmail.com>
2014-09-26 20:25:33 -0700

    dd

--- logincounter/models.py
+++ logincounter/models.py
@@ -1,62 +1,83 @@
-from django.db import models
+#!/usr/bin/env python
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django import forms
+from logincounter.models import User
+import json
+from django.views.decorators.csrf import csrf_exempt
 
-class User(models.Model):	
-	ERR_BAD_CREDENTIALS = -1
-	ERR_BAD_PASSWORD = -4
-	ERR_BAD_USERNAME = -3
-	ERR_USER_EXISTS = -2
+class UserForm(forms.Form):
 	MAX_PASSWORD_LENGTH = 128
 	MAX_USERNAME_LENGTH = 128
-	SUCCESS = 1
 	
-	user = models.CharField(max_length=MAX_USERNAME_LENGTH, unique=True)
-	password = models.CharField(max_length=MAX_PASSWORD_LENGTH, blank=True)
-	login_count = models.IntegerField(default=1)
+	user = forms.CharField(max_length=MAX_PASSWORD_LENGTH, required=True)
+	password = forms.CharField(max_length=MAX_USERNAME_LENGTH)
 
-	@classmethod
-	def login(self, data):
-		if data.get('user') and data.get('password'):
+@csrf_exempt
+def home(request):
+	form = UserForm()
+	return render(request, 'index.html', {'form': form})
+
+@csrf_exempt	
+def login(request):
+	if request.method == 'POST':
+		data = json.loads(request.body.encode(encoding='UTF-8'))
+		if data.get('user'):
 			try:
 				user = User.objects.get(user=data.get('user'))
 			except User.DoesNotExist as e:
-				return {'errCode': User.ERR_BAD_CREDENTIALS}
-				
-			if data.get('password') == user.password:
+				return HttpResponse(json.dumps({'errCode': User.ERR_BAD_CREDENTIALS}), content_type="application/json")
+			
+			if user.password == data.get('password'):
 				user.login_count += 1
 				user.save()
-				return {'errCode': User.SUCCESS, 'count': user.login_count}
-			return {'errCode': User.ERR_BAD_CREDENTIALS}
-		return {'errCode': User.ERR_BAD_CREDENTIALS}
+				return HttpResponse(json.dumps({'errCode': User.SUCCESS, 'count': user.login_count}), content_type="application/json")
+			else:
+				return HttpResponse(json.dumps({'errCode': User.ERR_BAD_CREDENTIALS}), content_type="application/json")
+		else:
+			return HttpResponse(json.dumps({'errCode': User.ERR_BAD_CREDENTIALS}), content_type="application/json")
+	return HttpResponse(json.dumps({}), content_type="application/json")
 
-	@classmethod
-	def add(self, data):
-		check = User.validate_user(data)
-		if(check == User.SUCCESS):
+@csrf_exempt      
+def add(request):
+	if request.method == 'POST':
+		data = json.loads(request.body.encode(encoding='UTF-8'))
+		if data.get('user') and len(data.get('user')) > 0 and len(data.get('user')) <= User.MAX_USERNAME_LENGTH:
+			if data.get('password') is None or len(data.get('password')) > User.MAX_PASSWORD_LENGTH:
+				return HttpResponse(json.dumps({'errCode': User.ERR_BAD_PASSWORD}), content_type="application/json")
 			try:
 				user = User.objects.get(user=data.get('user'))
-				return {'errCode': User.ERR_USER_EXISTS}
+				return HttpResponse(json.dumps({'errCode': User.ERR_USER_EXISTS}), content_type="application/json")
 			except User.DoesNotExist as e:
 				user = User(user=data.get('user'), password=data.get('password'))
 				user.save()
-				return {'errCode': User.SUCCESS, 'count': user.login_count}
-		return {'errCode': check}
-		
-	@classmethod
-	def validate_user(self, data):
-		if data.get('user') and data.get('password') is not None:
-			if len(data.get('user')) == 0 or len(data.get('user')) > User.MAX_USERNAME_LENGTH:
-				return User.ERR_BAD_USERNAME
-			elif len(data.get('password')) > User.MAX_PASSWORD_LENGTH:
-				return User.ERR_BAD_PASSWORD
-			return User.SUCCESS
-		elif data.get('password') is None:
-			return User.ERR_BAD_PASSWORD
-		return User.ERR_BAD_USERNAME
+				return HttpResponse(json.dumps({'errCode': User.SUCCESS, 'count': user.login_count}), content_type="application/json")
+		else:
+			return HttpResponse(json.dumps({'errCode': User.ERR_BAD_USERNAME}), content_type="application/json")
+	return HttpResponse(json.dumps({}), content_type="application/json")
 
-	@classmethod
-	def resetFixture(self):
+@csrf_exempt
+def resetFixture(request):
+	if request.method == 'POST':
 		User.objects.all().delete()
-		return {'errCode': User.SUCCESS}
-		
-	def __unicode__(self):
-		return self.user
+		return HttpResponse(json.dumps({'errCode': User.SUCCESS}), content_type="application/json")
+	return HttpResponse(json.dumps({}), content_type="application/json")
+	
+import os
+import subprocess
+ 
+@csrf_exempt
+def unitTests(request):
+	if request.method == 'POST':
+		nrFailed = 0
+		totalTests = 0
+		output = subprocess.check_output("python manage.py test logincounter; exit 0", shell=True, stderr=subprocess.STDOUT)
+		lines = output.splitlines()
+		for line in lines:
+			if line.startswith("Ran "):
+				totalTests = int(line.split(" ")[1])
+			elif line.startswith("FAILED (failures="):
+				nrFailed = line.split("=")[1][0:-1]
+		return HttpResponse(json.dumps({'nrFailed': nrFailed, 'output': output, 'totalTests': totalTests}), content_type="application/json")
+
+	return HttpResponse(json.dumps({}), content_type="application/json")
