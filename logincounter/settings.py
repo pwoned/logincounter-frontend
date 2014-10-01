219a433ef63d6191dba52fc8b10199f7f897a2a3
pwoned <peuanthinsan@gmail.com>
2014-10-01 01:25:16 -0700

    final

--- logincounter/settings.py
+++ logincounter/settings.py
@@ -56,12 +56,12 @@ WSGI_APPLICATION = 'logincounter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
-}
+}"""
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
@@ -91,7 +91,7 @@ TEMPLATE_DIRS = (
 #DATABASES = {}
 #DATABASES['default'] =  dj_database_url.config()
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'd3r0u58ugrthio',                      
@@ -100,7 +100,7 @@ TEMPLATE_DIRS = (
         'HOST': 'ec2-23-23-81-171.compute-1.amazonaws.com',
         'PORT': 5432
     }
-}"""
+}
 
 import sys
 if 'test' in sys.argv:
