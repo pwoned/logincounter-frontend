68bca0d96c5adb057b8a31e06ef0fa134ea469bb
pwoned <peuanthinsan@gmail.com>
2014-10-02 21:26:22 -0700

    extra credit

--- logincounter/static/js/logincounter.js
+++ logincounter/static/js/logincounter.js
@@ -18,16 +18,22 @@ function json_request(page, dict, success, failure) {
 	});
 }
 
+function show_login_page(user, count){
+	$('#login-message').html('Welcome, ' + user + '!');
+	$('#content').html('You have logged in ' + count + ' times.').show();
+	$('#inputs').hide();
+	$('#login-button').hide();
+	$('#add-button').hide();
+	$('#logout-button').show();
+	$('#login-box').attr('class', 'panel panel-success');
+}
+
 function update(data) {
 	switch(data.errCode) {
 		case SUCCESS:
-			$('#login-message').html('Welcome, ' + $('#user').val() + '!');
-			$('#content').html('You have logged in ' + data.count + ' times.').show();
-			$('#inputs').hide();
-			$('#login-button').hide();
-			$('#add-button').hide();
-			$('#logout-button').show();
-			$('#login-box').attr('class', 'panel panel-success');
+			show_login_page($('#user').val(), data.count);	
+			$.cookie("name", user);
+			$.cookie("count", data.count);
 			break;
 		case ERR_BAD_CREDENTIALS:
 			$('#login-message').html('Invalid username and password combination. Please try again.');
@@ -58,10 +64,32 @@ function logout() {
 	$('#add-button').show();
 	$('#logout-button').hide();
 	$('#login-box').attr('class', 'panel panel-default');
+	
+	$.removeCookie("name");
+	$.removeCookie("count");
 	return false;
 }
 
+function validate_user() {
+	user = $('#user').val();
+	if(user.length == 0 || user.length > MAX_USERNAME_LENGTH){
+		$('#login-message').html('The username should be non-empty and at most 128 characters long. Please try again.');
+		$('#login-box').attr('class', 'panel panel-danger');
+	}
+}
+
+function validate_password() {
+	password = $('#password').val();
+	if(password.length > MAX_PASSWORD_LENGTH){
+		$('#login-message').html('The password should be at most 128 characters long. Please try again.');
+		$('#login-box').attr('class', 'panel panel-danger');
+	}
+}
+
 $(document).ready(function() {
+	if($.cookie("name") && $.cookie("count")){
+		show_login_page($.cookie("name"), $.cookie("count"));
+	}
 	$('#login-button').click(function() {
 		user = $('#user').val();
 		password = $('#password').val();
@@ -75,4 +103,6 @@ $(document).ready(function() {
 		return false;
 	});
 	$('#logout-button').click(logout);
+	$('#user').blur(validate_user);
+	$('#password').blur(validate_password);
 });
